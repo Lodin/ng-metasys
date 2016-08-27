6d02c4dc14d1cbb0b03610f23b21cd1eea4528d3
Vlad Rindevich <rindevich.vs@gmail.com>
2016-08-27 14:15:34 +0300

    chore(): fix test start page minor issues

--- src/test.ts
+++ src/test.ts
@@ -5,11 +5,11 @@ import 'ts-helpers';
 
 declare var __karma__: any;
 
-__karma__.loaded = function () { return undefined; };
+__karma__.loaded = (): undefined => undefined;
 
 Promise.all([
   System.import('angular')
 ])
   .then(() => require.context('./', true, /\.spec\.ts/))
-  .then(context => context.keys().map(context))
+  .then((context: any) => context.keys().map(context))
   .then(__karma__.start, __karma__.error);
