94b756dabbfef9933949bba054c8b6fb2fa1f4f0
Vlad Rindevich <rindevich.vs@gmail.com>
2017-02-12 20:05:42 +0300

    fix(@Bind): get 100% coverage

--- src/extensions/bind-decorator.ts
+++ src/extensions/bind-decorator.ts
@@ -1,16 +1,10 @@
 import * as tokens from '../core/tokens';
 
 type BindDecorator =
-  (type: string) => (target: any, property: string, descriptor?: PropertyDescriptor) => void;
+  (type: string) => (target: any, property: string) => void;
 const Bind: BindDecorator =
   type =>
-    (target, property, descriptor) => {
-      if (descriptor) {
-        descriptor.writable = true;
-        descriptor.configurable = true;
-        descriptor.enumerable = true;
-      }
-
+    (target, property) => {
       if (!Reflect.hasMetadata(tokens.binding, target)) {
         Reflect.defineMetadata(tokens.binding, {[property]: type}, target);
         return;
