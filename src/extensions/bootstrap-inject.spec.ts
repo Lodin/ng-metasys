b1d6d0ab327d121fea65434a731174e98956625d
Vlad Rindevich <rindevich.vs@gmail.com>
2017-02-14 00:57:13 +0300

    refactor(inject): improve tests & add 100% coverage

--- src/extensions/bootstrap-inject.spec.ts
+++ src/extensions/bootstrap-inject.spec.ts
@@ -4,9 +4,14 @@ import bootstrapInject from './bootstrap-inject';
 
 describe('Function `bootstrapInject`', () => {
   class MockInjectDeclaration {}
-  class TestDeclaration {
-    public static config() {}
-  }
+
+  type Clear = (declaration: any) => void;
+  const clear: Clear =
+    declaration => {
+      Reflect.deleteMetadata(tokens.inject.self, declaration.prototype);
+      Reflect.deleteMetadata(tokens.inject.method, declaration);
+      Reflect.deleteMetadata(tokens.inject.param, declaration);
+    };
 
   let ngModule: angular.IModule;
 
@@ -14,13 +19,11 @@ describe('Function `bootstrapInject`', () => {
     ngModule = angular.module('TestModule', []);
   });
 
-  afterEach(() => {
-    Reflect.deleteMetadata(tokens.inject.self, TestDeclaration.prototype);
-    Reflect.deleteMetadata(tokens.inject.method, TestDeclaration);
-    Reflect.deleteMetadata(tokens.inject.param, TestDeclaration);
-  });
-
   it('should create DeclarationInjector instance with common injections', () => {
+    class TestDeclaration {
+      public static config() {}
+    }
+
     Reflect.defineMetadata(tokens.inject.self, [
       '$http',
       MockInjectDeclaration
@@ -32,9 +35,15 @@ describe('Function `bootstrapInject`', () => {
 
     injector.injectCommon(TestDeclaration);
     expect((TestDeclaration as any).$inject).toEqual(['$http', 'MockInjectDeclaration']);
+
+    clear(TestDeclaration);
   });
 
   it('should create DeclarationInjector instance with method injections', () => {
+    class TestDeclaration {
+      public static config() {}
+    }
+
     Reflect.defineMetadata(tokens.inject.method, {
       config: ['$http', MockInjectDeclaration]
     }, TestDeclaration);
@@ -47,9 +56,15 @@ describe('Function `bootstrapInject`', () => {
     injector.injectMethods(TestDeclaration, 'config');
 
     expect((TestDeclaration.config as any).$inject).toEqual(['$http', 'MockInjectDeclaration']);
+
+    clear(TestDeclaration);
   });
 
   it('should create DeclarationInjector instance with constructor parameter injections', () => {
+    class TestDeclaration {
+      public static config() {}
+    }
+
     Reflect.defineMetadata(tokens.inject.param, [
       '$http',
       MockInjectDeclaration
@@ -61,5 +76,38 @@ describe('Function `bootstrapInject`', () => {
 
     injector.injectCommon(TestDeclaration);
     expect((TestDeclaration as any).$inject).toEqual(['$http', 'MockInjectDeclaration']);
+
+    clear(TestDeclaration);
+  });
+
+  describe('should set $inject property to empty array', () => {
+    it('if there is no methods', () => {
+      class TestDeclaration {
+        public static config() {}
+      }
+
+      const injector = bootstrapInject(TestDeclaration);
+      expect(injector.hasMethods).not.toBeTruthy();
+
+      injector.injectMethods(TestDeclaration, 'config');
+      expect((TestDeclaration.config as any).$inject).toEqual([]);
+
+      clear(TestDeclaration);
+    });
+
+    it('if there is no methods', () => {
+      class TestDeclaration {
+        public static config() {}
+      }
+
+      const injector = bootstrapInject(TestDeclaration);
+      expect(injector.hasCommon).not.toBeTruthy();
+
+      injector.injectCommon(TestDeclaration);
+
+      expect((TestDeclaration as any).$inject).toEqual([]);
+
+      clear(TestDeclaration);
+    });
   });
 });
