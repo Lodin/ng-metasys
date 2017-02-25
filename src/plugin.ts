ca8032bbb0d98f646c53c506c6dea59998e54f85
Vlad Rindevich <rindevich.vs@gmail.com>
2017-02-25 23:34:01 +0300

    feat(plugin system): add exporting built-in tokens

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,2 +1,4 @@
+export * from './core/tokens';
+export * from './core/token-lists';
 export {default as registerPlugin} from './core/plugin-registry';
 export {defineMetadata, getPluginMetadata} from './core/reflection';
