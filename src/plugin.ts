166af110804d64c152d958357ebb23357678eef4
Vlad Rindevich <rindevich.vs@gmail.com>
2017-02-27 01:26:37 +0300

    fix(plugin system): add exporting tokens in named groups

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,6 @@
-export * from './core/tokens';
-export * from './core/token-lists';
+import * as tokens from './core/tokens';
+import * as tokenLists from './core/token-lists';
+
+export {tokens, tokenLists};
 export {default as registerPlugin} from './core/plugin-registry';
 export {defineMetadata, getPluginMetadata} from './core/reflection';
