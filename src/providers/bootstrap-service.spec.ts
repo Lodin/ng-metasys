3bd3e30e60881ddc196cc87c4eb4b9b8818701ca
Vlad Rindevich <rindevich.vs@gmail.com>
2017-03-12 17:07:53 +0300

    refactor(providers): remove unnecessary imports

--- src/providers/bootstrap-service.spec.ts
+++ src/providers/bootstrap-service.spec.ts
@@ -1,4 +1,3 @@
-import * as angular from 'angular';
 import * as NgmsReflect from '../core/reflection';
 import * as tokens from '../core/tokens';
 import * as bootstrapInject from '../extensions/bootstrap-inject';
